729aa8d6de4c851477705c4cce18c741d081dfbc
Ngo Iok Ui <wusyong9104@gmail.com>
2021-08-21 14:18:55 +0800

    Update soup-rs

--- soup/src/lib.rs
+++ soup/src/lib.rs
@@ -1,19 +1,6 @@
 #![allow(dead_code)]
 
-#[macro_use]
-extern crate glib;
-extern crate glib_sys;
-extern crate gio;
-extern crate gio_sys;
-extern crate gobject_sys;
 extern crate soup_sys as ffi;
-extern crate futures_util;
-extern crate fragile;
-
-extern crate libc;
-#[macro_use]
-extern crate bitflags;
-
 
 /// Asserts that this is the main thread and `gtk::init` has been called.
 macro_rules! assert_initialized_main_thread {
